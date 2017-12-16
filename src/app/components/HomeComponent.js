c80b6bb0bcd26237ed3adad7ca07d9f8303e8dee
Hemant Sankhla <hemantsan@cybage.com>
2017-12-16 17:01:48 +0530

    Props, State and Event handeling added.

--- src/app/components/HomeComponent.js
+++ src/app/components/HomeComponent.js
@@ -1,10 +1,29 @@
 import React, { Component } from "react";
 
 class HomeComponent extends Component {
+
+    constructor(props) {
+        super();
+        this.state = {
+            age: props.initialAge
+        };
+        // this.onSetAge = this.onSetAge.bind(this);
+    }
+    
+    onSetAge() {
+        this.setState({
+            age: this.state.age + 3,
+        });
+    }
+
     render() {
         return (
-            <div>
+            <div className="jumbotron">
                 <p>I am a new Component.</p>
+                <p>Name : {this.props.name}</p>
+                <p>Age : {this.state.age}</p>
+                <p>Hobbies : {this.props.user.hobbies.map( (hob) => {return hob + ', '; }) }</p>
+                <button className="btn-primary btn" onClick={() => this.onSetAge()}>Set Age</button>
             </div>
         );
     }
--- src/app/index.js
+++ src/app/index.js
@@ -4,14 +4,20 @@ import HeaderComponent from "./components/HeaderComponent";
 import HomeComponent from "./components/HomeComponent";
 
 class App extends Component {
+
     render() {
+        var user = {
+            name: "Luna",
+            hobbies: ["Sports", "Horse Riding"]
+        }
+
         return ( 
             <div className="container">
-                <div className="row">
-                    <HeaderComponent/>
-                </div>
-                <div className="row">                
-                    <HomeComponent/>
+                <HeaderComponent/>
+                <div className="row">     
+                    <div className="col-md-12">           
+                        <HomeComponent name={"Vic"} initialAge={25} user={user}/>
+                    </div>
                 </div>
             </div>
         );
